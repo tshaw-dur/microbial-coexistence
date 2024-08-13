d47be46883571f97a77d6a0d94911e8f4b1db810
tshaw-dur <thomas.e.shaw@durham.ac.uk>
2024-08-13 10:25:53 +0100

    Update 2parameter.m

--- 2parameter.m
+++ 2parameter.m
@@ -11,39 +11,45 @@ J = jacobian(F,vars);
 %M1
 function F = m1solve(x,A11,I,D,P1)
     M = x(1);
-    F(1) = M*(D+P1*(sech(M-5)-0.2)/A11) - I;
+    F(1) = D+P1*(sech(M-5)-0.2)/A11 - I/M;
 end
 
 %M2
 function G = m2solve(x,A22,I,D,P2)
     M = x(1);
-    G(1) = M*(D+P2*(sech(M-6.5)-0.2)/A22) - I;
+    G(1) = D+P2*(sech(M-6.5)-0.2)/A22 - I/M;
 end
 
 %Coexistence
 function H = m3solve(x,A11,A12,A21,A22,I,D,P1,P2)
     M = x(1);
-    H(1) = I-D*M-(M*(sech(M-5)-0.2)*(P1*A22-P2*A21)/(A11*A22-A21*A12))-(M*(sech(M-6.5)-0.2)*(P2*A11-P1*A12)/(A11*A22-A21*A12));
+    H(1) = D+(sech(M-5)-0.2)*(P1*A22-P2*A21)+(sech(M-6.5)-0.2)*(P2*A11-P1*A12) - I/M;
 end
 
 % CHOOSE YOUR PARAMETERS %
 % HIDE THE TWO YOU WANT TO PLOT %
-A11 = 0.4;
+A11 = 5;
 A12 = 0;
 A21 = 0;
-A22 =  0.4;
+A22 =  1;
 %I = 21.6;
 %D = 2.5;
-P1 = 4.1;
-P2 = 0.15;
+P1 = 10;
+P2 = 0.5;
 
 % CHOOSE THE LENGTH AND WIDTH OF THE PLOTS %
 % CHOOSE THE PARAMETERS YOU WANT TO PLOT%
-xlen = 20;
-ylen = 2;
-num_points = 25;
-for I = linspace(0.1,xlen,num_points)
-    for D = linspace(0.1,ylen,num_points)
+ymin = 1;
+ymax = 5;
+xmin = 1;
+xmax = 40;
+num_points = 200;
+
+colours = zeros(1,num_points);
+
+for D = linspace(ymin,ymax,num_points) %y-axis
+    Icolours = [];
+    for I = linspace(xmin,xmax,num_points) %x-axis
         
         equilibria = [0,0,0]; %Create a list for equilibria%
         
@@ -62,9 +68,9 @@ for I = linspace(0.1,xlen,num_points)
         M1range = linspace(2,8,5);
         for a = 1:length(M1range)
             x0 = M1range(a);
-            solved = fsolve(@(x) m1solve(x,A11,I,D,P1), x0);
+            solved = fsolve(@(x) m1solve(x,A11,I,D,P1), x0); %Solve for M1
             %disp(solved)
-            equil = [(sech(solved-5)-0.2)/A11,0,solved];
+            equil = [(sech(solved-5)-0.2)/A11,0,solved]; %Equilibrium point
             %disp(equil)
             equil = round(equil, 3); %Avoids rounding errors%
                     if equil(1) >= 0 && equil(2) >=0 && equil(3) >=0 && ismember(equil, equilibria, 'rows')==0 
@@ -83,9 +89,9 @@ for I = linspace(0.1,xlen,num_points)
         M2range = linspace(4,10,5);
         for a = 1:length(M2range)
             x0 = M2range(a);
-            solved = fsolve(@(x) m2solve(x,A22,I,D,P2), x0);
+            solved = fsolve(@(x) m2solve(x,A22,I,D,P2), x0); %Solve for M2
             %disp(solved)
-            equil = [0,(sech(solved-6.5)-0.2)/A22,solved];
+            equil = [0,(sech(solved-6.5)-0.2)/A22,solved]; %Equilibrium point
             %disp(equil)
             equil = round(equil, 3); %Avoids rounding errors%
                     if equil(1) >= 0 && equil(2) >=0 && equil(3) >=0 && ismember(equil, equilibria, 'rows')==0 
@@ -104,7 +110,7 @@ for I = linspace(0.1,xlen,num_points)
         M3range = linspace(0.1,10,10);
         for a = 1:length(M3range)
             x0 = M3range(a);
-            solved = fsolve(@(x) m3solve(x,A11,A12,A21,A22,I,D,P1,P2), x0);
+            solved = fsolve(@(x) m3solve(x,A11,A12,A21,A22,I,D,P1,P2), x0); %Solve for M3
             %disp(solved)
             equil = [(A22*(sech(solved-5)-0.2)-A12*(sech(solved-6.5)-0.2))/(A11*A22-A12*A21),(A11*(sech(solved-6.5)-0.2)-A21*(sech(solved-5)-0.2))/(A11*A22-A12*A21),solved];
             %disp(equil)
@@ -123,9 +129,8 @@ for I = linspace(0.1,xlen,num_points)
 
         
         equilibria(1,:) = []; %Gets rid of initial [0,0,0]
-        disp(equilibria) %Shows the feasible, stable equilibria for this set of parameters%
+        %disp(equilibria) %Shows the feasible, stable equilibria for this set of parameters%
 
-        %Assign the co-ordinate a 4-digit base 3 number.
         %Each digit represents the number of stable equilibria of that type
         colour = [0,0,0,0]; %[co,m2,m1,ext]
         for k = 1:length(equilibria(:,1))
@@ -143,61 +148,79 @@ for I = linspace(0.1,xlen,num_points)
             end
         end
         %disp(colour)
-        number = colour(4) + (3*colour(3)) + (9*colour(2)) + (27*colour(1));
-        %disp(number)
-        finalcolour=[0,0,0];
-
-        %Assigning each type of stability a colour to plot it with
-        if number == 0 
-            finalcolour = [0,0,0]; %unstable/infeasible = black
-        end
-        if number == 1 
-            finalcolour = [1,0,0]; %ext = red
-        end
-        if number == 3 
-            finalcolour = [0.6,1,0]; %m1 = light green 
-        end
-        if number == 9 
-            finalcolour = [0.12,0.58,0]; %m2 = dark green
-        end
-        if number == 27 
-            finalcolour = [0,0,1]; %co = blue
-        end
-        if number == 4 
-            finalcolour = [0.97,1,0.55]; %ext-m1 = light yellow
-        end
-        if number == 10 
-            finalcolour = [0.84,0.90,0]; %ext-m2 = dark yellow
-        end
-        if number == 28 
-            finalcolour = [0.60,0,1]; %ext-co = purple
-        end
-        if number == 12 
-            finalcolour = [0,1,0]; %m1-m2 = green
-        end
-        if number == 15 
-            finalcolour = [0.12,0.89,0]; %m1-m1-m2 = slightly light green
-        end
-        if number == 21 
-            finalcolour = [0.15,0.72,0]; %m1-m2-m2 = slightly dark green
-        end
-        if number == 13
-            finalcolour = [0.93,1,0]; %ext-m1-m2 = yellow
-        end
         
-
-        %These colours are currently only complete for A12=A21=0.
+        if colour == [0,0,0,0] %black
+            finalcolour = 0;
+        end
+        if colour == [0,0,0,1] %red
+            finalcolour = 1;
+        end
+        if colour == [0,0,1,0] %light green
+            finalcolour = 2;
+        end
+        if colour == [0,1,0,0] %dark green
+            finalcolour = 3;
+        end
+        if colour == [1,0,0,0] %blue
+            finalcolour = 4;
+        end
+        if colour == [0,0,1,1] %light yellow
+            finalcolour = 5;
+        end
+        if colour == [0,1,0,1] %dark yellow
+            finalcolour = 6;
+        end
+        if colour == [1,0,0,1] %purple
+            finalcolour = 7;
+        end
+        if colour == [0,1,1,0] %green
+            finalcolour = 8;
+        end
+        if colour == [0,1,2,0] %slightly light green
+            finalcolour = 9;
+        end
+        if colour == [0,2,1,0] %slightly dark green
+            finalcolour = 10;
+        end
+        if colour == [0,1,1,1] %yellow
+            finalcolour = 11;
+        end
+        Icolours = [Icolours, finalcolour];
         
-        hold on;
-        %CHOOSE THE PARAMETERS YOU WANT TO PLOT%
-        plot(I, D, '.', 'MarkerEdgeColor', finalcolour, 'MarkerFaceColor', finalcolour);
-        hold off;
-        axis([0 xlen 0 ylen]);
-        xlabel('I');
-        ylabel('D');
-        grid on;
     end
+    %disp(Icolours)
+    colours=[colours;Icolours];
 end
 
+colours(1,:) = [];
+disp(colours)
+%disp(length(colours))
 
+customColorMap = [
+    0, 0, 0;        %0 = black
+    1, 0, 0;        %1 = red
+    0.6, 1, 0;      %2 = light green
+    0.12, 0.58, 0;  %3 = dark green
+    0, 0, 1;        %4 = blue
+    0.97, 1, 0.55;  %5 = light yellow
+    0.84, 0.90, 0;  %6 = dark yellow
+    0.60, 0, 1;     %7 = purple 
+    0, 1, 0;        %8 = green
+    0.12, 0.89, 0;  %9 = slightly light green
+    0.15, 0.72, 0   %10 = slightly dark green
+    %0.93, 1, 0      %11 = yellow
+    ];
 
+%disp(length(customColorMap))
+
+Dspace = linspace(ymin,ymax,num_points);
+Ispace = linspace(xmin,xmax,num_points);
+
+%disp(Dspace)
+%disp(Ispace)
+
+colormap(customColorMap)
+set(gca, 'CLim', [0 8]);
+imagesc(Ispace,Dspace,colours)
+axis xy
+colorbar;
